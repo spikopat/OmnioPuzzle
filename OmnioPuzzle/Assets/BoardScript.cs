0abc465096b024ba184a661f99c5331ccf0b2af6
Serdar Doğan <serdar.dogaan@gmail.com>
2020-09-02 21:16:35 +0300

    Level oluşturup onu ekrana spawnlama işlemleri tamamlandı.

---
+++ OmnioPuzzle/Assets/BoardScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScript : MonoBehaviour {
+
+    MeshRenderer cubeMeshRenderer;
+    public Transform cubeTransform;
+
+    public Vector3 cubeMeshSize;
+
+    public Vector3 topLeftPoint;
+
+    private void Awake() {
+        cubeMeshRenderer = cubeTransform.GetComponent<MeshRenderer>();
+        cubeMeshSize = cubeMeshRenderer.bounds.size;
+        topLeftPoint = new Vector3(cubeTransform.position.x - (cubeMeshSize.x / 2), cubeTransform.position.y, cubeTransform.position.z + (cubeMeshSize.z / 2));
+        Debug.Log(topLeftPoint);
+    }
+}
--- OmnioPuzzle/Assets/Scripts/Managers/GameSceneSpawnManager.cs
+++ OmnioPuzzle/Assets/Scripts/Managers/GameSceneSpawnManager.cs
@@ -2,20 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LevelEnum {
+    Level1,
+    Level2
+}
 public class GameSceneSpawnManager : MonoBehaviour {
 
+    public LevelEnum levelEnum;
+
+    int[,] level1 = { 
+        { 2, 1, 1, 2 }, 
+        { 1, 1, 2, 0 }, 
+        { 1, 2, 1, 1 }
+    };
+    int[,] level2 = {
+        { 1, 2, 1, 1, 2},
+        { 1, 1, 0, 2, 3},
+        { 2, 1, 2, 1, 1}
+    };
+
     #region FROM_INSPECTOR
+    public GameObject spawnPoint;
+    public GameObject boardWood;
+    public GameObject fruitWithFork;
+
+    [Header("Board")]
+    public BoardScript boardScript;
+
+    [SerializeField]
+    float offsetValue;
     #endregion
 
+    Puzzle[,] grid;
+    int z, x;
+    float boardWoodSize;
+    float fruitWithForkSize;
+
+    //Seçilmiş olan levelin değerlerini tutar.
+    int[,] selectedLevel;
+    public Transform lastSpawnedObject;
+
     void SpawnLevel() {
+        GameObject spawnedObject;
+        for (int i = 0; i < z; i++) {
+            for (int j = 0; j < x; j++) {
+                spawnedObject = Instantiate(spawnPoint, new Vector3(
+                        boardScript.topLeftPoint.x + ((boardWoodSize * (j+1)) + (offsetValue * j)),
+                        boardScript.topLeftPoint.y, 
+                        boardScript.topLeftPoint.z - ((boardWoodSize * (i+1)) + (offsetValue * i))),
+                        Quaternion.identity
+                    );
+                spawnedObject.name = "[" + i + "][" + j + "]";
+                spawnedObject.transform.SetParent(boardScript.cubeTransform);
+                Puzzle spawnedPuzzle = spawnedObject.GetComponent<Puzzle>();
+                spawnedPuzzle.SetBlocks(selectedLevel[i, j]);
+                //grid[i,j] = spawnedPuzzle;
+                lastSpawnedObject = spawnedObject.transform;
+            }
+        }
+        Instantiate(fruitWithFork, new Vector3(lastSpawnedObject.position.x, lastSpawnedObject.position.y + (boardWoodSize / 4), lastSpawnedObject.position.z), Quaternion.Euler(0, 90, 0));
     }
 
     #region UNITY_FUNCTIONS
-    private void Awake() {
-        
-    }
     private void Start() {
+        if (levelEnum == LevelEnum.Level1) {
+            z = level1.GetLength(0);
+            x = level1.GetLength(1);
+            selectedLevel = level1;
+        }
+        else if (levelEnum == LevelEnum.Level2) {
+            selectedLevel = level2;
+            z = level2.GetLength(0);
+            x = level2.GetLength(1);
+        }
+        grid = new Puzzle[z, x];
+        GetBounds();
         SpawnLevel();
     }
     #endregion
+
+    void GetBounds() {
+        boardWoodSize = boardWood.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.x;
+        fruitWithForkSize = fruitWithFork.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.y;
+    }
 }

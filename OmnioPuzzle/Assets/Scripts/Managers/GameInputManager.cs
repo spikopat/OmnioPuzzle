6f8f392965023820fe541a08a6d3c82844495e9f
Serdar Doğan <serdar.dogaan@gmail.com>
2020-09-02 21:19:51 +0300

    Ara commit

--- OmnioPuzzle/Assets/Scripts/Managers/GameInputManager.cs
+++ OmnioPuzzle/Assets/Scripts/Managers/GameInputManager.cs
@@ -7,19 +7,49 @@ public enum InputMethod {
 }
 public class GameInputManager : MonoBehaviour {
 
-
     public InputMethod inputType;
 
-    private void PCInput() {
+    void MoveForward() {
+        Debug.Log("Forward");
+    }
 
+    void MoveBack() {
+        Debug.Log("Back");
+    }
+
+    void MoveLeft() {
+        Debug.Log("Left");
+    }
+
+    void MoveRight() {
+        Debug.Log("Right");
+    }
+
+    private void PCInput() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            MoveForward();
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+            MoveBack();
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            MoveRight();
+        }
     }
 
     private void TouchInput() {
-        
+       
     }
 
     private void Update() {
-
+        #if UNITY_EDITOR
+            PCInput();
+        #elif UNITY_ANDROID
+            TouchInput();
+        #endif
     }
 
 }

a990dc5e4b754ec71443a8380ec9410fded3675b
Serdar Doğan <serdar.dogaan@gmail.com>
2020-09-06 17:13:04 +0300

    Çikolata izi bırakma animasyonu tamamlandı.

---
+++ OmnioPuzzle/Assets/Scripts/BoardWoodScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardWoodScript : MonoBehaviour {
+    public GameObject spawnableChocolate;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "FruitChocolate") {
+            GameObject spawnedGameObject = Instantiate(spawnableChocolate, new Vector3(0, 0, 0), Quaternion.identity, transform);
+        }
+    }
+}

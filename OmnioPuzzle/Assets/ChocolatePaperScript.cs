0f709d3599c63db483c2a02f8de3e3bc582010ea
Serdar Doğan <serdar.dogaan@gmail.com>
2020-09-07 21:45:50 +0300

    Particlelar eklendi ve prefabları oluşturuldu.

--- OmnioPuzzle/Assets/ChocolatePaperScript.cs
+++ OmnioPuzzle/Assets/ChocolatePaperScript.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 
 public class ChocolatePaperScript : MonoBehaviour {
 
+    [SerializeField]
+    GameObject particles;
+    [SerializeField]
+    GameObject confitiParticles;
+
     //Kendi etrafında döndürür.
     public void FirstState() {
-        transform.DORotate(new Vector3(0, 270, 0), 3, RotateMode.Fast).SetLoops(1, LoopType.Incremental).SetEase(Ease.InSine).OnComplete(()=> SecondState());
+
+        //StartCoroutine(StartParticles());
+
+        transform.DORotate(new Vector3(0, 270, 0), 3, RotateMode.Fast)
+            .SetLoops(1, LoopType.Incremental)
+            .SetEase(Ease.InSine)
+            .OnComplete(()=> SecondState());
     }
 
     //Yukarı kaldırarak döndürür. Yukarı kaldırma bittiğinde sallar.
     void SecondState() {
-        transform.DOMoveY(0.28f, 0.5f).OnComplete(() => transform.DOShakeRotation(0.5f, 45, 10, 90, false));
-        transform.DORotate(new Vector3(0, 180, 0), 0.2f, RotateMode.FastBeyond360).SetLoops(5, LoopType.Incremental).SetEase(Ease.Linear).OnComplete(() => ThirdState());
+
+        transform.DOMoveY(0.28f, 0.5f)
+            .OnComplete(() => transform.DOShakeRotation(0.5f, 45, 10, 90, false));
+
+        transform.DORotate(new Vector3(0, 180, 0), 0.2f, RotateMode.FastBeyond360)
+            .SetLoops(5, LoopType.Incremental)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => ThirdState());
     }
 
     //Aşağı indirerek döndürür.
     void ThirdState() {
-        transform.DORotate(new Vector3(0, 0, 0), 1, RotateMode.FastBeyond360).SetLoops(1, LoopType.Incremental).SetEase(Ease.OutQuad);
+        //confitiParticles.SetActive(true);
+        //confitiParticles.GetComponent<ParticleSystem>().Play();
+
+        transform.DORotate(new Vector3(0, 0, 0), 1, RotateMode.FastBeyond360)
+            .SetLoops(1, LoopType.Incremental)
+            .SetEase(Ease.OutQuad);
+
         transform.DOMoveY(0.25f, 1);
     }
 
+    IEnumerator StartParticles() {
+        yield return new WaitForSeconds(2);
+        particles.SetActive(true);
+    }
 }
---
+++ OmnioPuzzle/Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Update() {
+        if (transform.position.y >= 0.0276f) {
+            transform.position = new Vector3(transform.position.x, transform.position.y - (Time.deltaTime / 20), transform.position.z);
+        }
+    }
+
+}
